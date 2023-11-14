83bffc2b1588db1e9c7405daa0dac5fd895aed8c
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-14 21:11:18 +0900

    リンクのホバー時に不透明度をアニメーションで変更する

--- js/script.js
+++ js/script.js
@@ -6,4 +6,14 @@ $(function () {
         autoplaySpeed: 5000,
         arrows: false,
     });
+
+    // リンクのホバー
+    $('a').hover(
+        function() {
+            $(this).animate({'opacity':0.6}, 300);
+        },
+        function() {
+            $(this).animate({'opacity':1.0},300);
+        }
+    );
 });

5be378e60ddf5c656604c2f9cd693816fb55a06c
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-14 21:45:01 +0900

    スクロールしたときにTOPに戻るボタンを表示させる

--- js/script.js
+++ js/script.js
@@ -16,4 +16,17 @@ $(function () {
             $(this).animate({'opacity':1.0},300);
         }
     );
+
+    // topに戻るボタン
+    $(window).on('scroll', () => {
+        // スクロール量が100pxを超えたらボタンを表示する
+        const scrollSize = $(this).scrollTop();
+        if (scrollSize > 100){
+            $('.back_button').css('display', 'inline');
+             $('.back_button').fadeIn();
+        } else {
+            $('.back_button').css('display', 'none');
+            // $('.back_button').fadeOUT();
+        }
+    });
 });
